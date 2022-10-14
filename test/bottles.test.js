a260e64284d3e0f629c7dc973d3c02d5d6c8e061
Oscar Mellado <empanaisimo@gmail.com>
2022-10-14 14:36:43 +0200

    Add missing skip in test

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -52,7 +52,7 @@ Take one down and pass it around, 97 bottles of beer on the wall.
     expect(new Bottles().verses(99, 98)).toBe(expected);
   });
 
-  test('a few verses', () => {
+  test.skip('a few verses', () => {
     const expected =
       `2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
