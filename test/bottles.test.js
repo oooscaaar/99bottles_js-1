ed20d72de2087851e854ab91de516fb8af2f2be5
Oscar M <empanaisimo@gmail.com>
2022-10-15 14:27:06 +0200

    Seventh test green

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -50,10 +50,10 @@ Take one down and pass it around, 97 bottles of beer on the wall.`;
     const expected =
       `2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
-      
+
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
-      
+
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.`;
     expect(new Bottles().verses(2, 0)).toBe(expected);
